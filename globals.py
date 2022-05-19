5f535e7c8a06094e1c2e7cc07533a0b4045bd7c0
Omer Caldararu <82719317+omercalder123@users.noreply.github.com>
2022-05-19 14:02:22 +0300

    Changed the gold off model to regression

--- globals.py
+++ globals.py
@@ -15,7 +15,7 @@ n_cores_for_gold_off = 10
 # this variable stores the name of the model used by gold_off_func the file needs to be in the code directory
 # important: set n_process to 1 when debugging, otherwise the code can get stuck
 # it's used in Distance_matrix_and_UPGMA.gold_off_func:
-xgb_model_name = "GS_TL_add_classifier_xgb_model_fold_0_without_Kfold_imbalanced.xgb"
+xgb_model_name = "regression_union_log_max.xgb"
 
 # in case you need to activate conda when using remote debug
 CONDA = "source /groups/itay_mayrose/udiland/miniconda3/etc/profile.d/conda.sh; conda activate crispys"
