c4e1e231ea490dd80b560ab7109fd37b2a443991
Omer Caldararu <82719317+omercalder123@users.noreply.github.com>
2022-06-16 12:52:15 +0300

    - added "random.seed" to the start of CRISPys_main - Fixed some docstrings and changed some variable names in Metric.py - Added a note to globals.py n_cores stating that 10 cores are currently required for gold off. This will change when we find how to reduce the cpu demand to 1.

--- Metric.py
+++ Metric.py
@@ -14,23 +14,23 @@ def pos_in_metric_general_single_batch(list_of_targets: List, metric_sequences_l
 	This function is called from pos_in_metric_general for cases where the scoring function
 	takes a list of several targets, rather than a single target.
 	This function calls the distance_function on all the targets in a single batch.
-	:param list_of_targets: a list of targets
-	:param metric_sequences_list: a list of sampled sequences used for the distance transformation calculation
-	:param distance_function: the distance function
+	:param list_of_targets: a list of targets.
+	:param metric_sequences_list: a list of sampled sequences used for the metric calculation.
+	:param distance_function: the distance function.
 	:return: a list of vectors, each of length globals.vector_size_cutoff.
 	input_target_list = [target1, target1, target1,..., target2 ...]
-	input_other_targets_list = [metric_sequence1, metric_sequence2, metric_sequence3, ...,metric_sequencei, ...]
+	input_metric_sequences_list = [metric_sequence1, metric_sequence2, metric_sequence3, ...,metric_sequencei, ...]
 	concatenated_vectors_list = the result of the distance function, which is then divided into smaller lists.
 	output_format : [distance_function(metric_sequence1,target_1),distance_function(metric_sequence2,target_1),...]
 	"""
 	input_target_list = []
-	input_other_targets_list = []
+	input_metric_sequences_list = []
 	for target in list_of_targets:
 		input_target_list.extend([target] * len(metric_sequences_list))
 		# update the PAM in each constant target according to the PAM of the target
 		metric_sequences_with_pam = [f"{t}{target[20:]}" for t in metric_sequences_list]
-		input_other_targets_list.extend(metric_sequences_with_pam)
-	concatenated_vectors_list = distance_function(input_other_targets_list, input_target_list)
+		input_metric_sequences_list.extend(metric_sequences_with_pam)
+	concatenated_vectors_list = distance_function(input_metric_sequences_list, input_target_list)
 	output_vectors_list = []
 	for i in range(0, len(concatenated_vectors_list), len(metric_sequences_list)):
 		output_vectors_list.append(concatenated_vectors_list[i:i + len(metric_sequences_list)])
@@ -63,10 +63,10 @@ def pos_in_metric_general(list_of_targets: List, distance_function) -> List:
     elif distance_function == Distance_matrix_and_UPGMA.ccTop or distance_function == Distance_matrix_and_UPGMA.MITScore:
         list_of_vectors = []
         for target in list_of_targets:
-            target_vector = []
+            score_vector = []
             for sequence in metric_sequences_list:
-                target_vector.append(distance_function(sequence, target))
-            list_of_vectors.append(target_vector)
+                score_vector.append(distance_function(sequence, target))
+            list_of_vectors.append(score_vector)
         return list_of_vectors
 
 
@@ -79,7 +79,6 @@ def create_list_of_metric_sequences(list_of_targets: List) -> List:
 	If the number of targets is larger or equal to this variable, shuffle the list of targets and take only the first
 	vector_size_cutoff targets.
 	:param list_of_targets: a list of targets
-	:param list_of_targets: a list of targets.
 	:return: a list of sampled sequences of length @globals.vector_size_cutoff.
 	"""
 	if len(list_of_targets) >= vector_size_cutoff:
--- globals.py
+++ globals.py
@@ -11,9 +11,8 @@ N_genes_in_node = 10
 vector_size_cutoff = 1000
 
 # this variable will determine the number of cores used for the multi_processing used by gold_off.predict
-# important: set n_process to 1 when debugging, otherwise the code can get stuck
-# it's used in Distance_matrix_and_UPGMA.gold_off_func:
-n_cores_for_gold_off = 1
+# important: currently, gold off only supports 10 n_cores.
+n_cores_for_gold_off = 10
 
 # This variable stores the name of the model used by gold_off_func the file needs to be in the code directory.
 # It's used in Distance_matrix_and_UPGMA.gold_off_func:
