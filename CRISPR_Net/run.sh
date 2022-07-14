9c65aa4b958198f4f88a2268771ac9037dd06f50
udiland <udiland@tauex.tau.ac.il>
2022-07-14 13:39:23 +0300

    add crispr_net folder

---
+++ CRISPR_Net/run.sh
@@ -0,0 +1,11 @@
+# run CRISPR-Net to score gRNA-target pairs
+python /code/CRISPR_Net.py /code/input_examples/on_off_seq_pairs.txt
+
+# run CRISPR-Net-Aggregate to produce a overall off-target score for a gRNA
+python /code/CRISPR_Net_Aggregate.py /code/input_examples/aggregate_example_GACCTTGCATTGTACCCGAG.csv
+
+# evaluate CRISPR-Net based on the dataset in /data
+python /code/evaluate_CRISPR_Net.py
+
+# train and testing CRISPR-Net-Aggregate on the datasets in /data/Aggregate_dataset
+python /code/evaluate_CRISPR_Net_Aggregate.py
