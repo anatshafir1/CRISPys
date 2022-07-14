56d0d0f0642e6a6bb45671149ba2f1d196f537cc
JosefBrook <josefbrook@mail.tau.ac.il>
2022-07-14 11:37:16 +0300

    stage0 and stage1 updated

--- globals.py
+++ globals.py
@@ -21,5 +21,10 @@ xgb_model_name = "regression_union_log_max.xgb"
 # in case you need to activate conda when using remote debug
 CONDA = "source /groups/itay_mayrose/udiland/miniconda3/etc/profile.d/conda.sh; conda activate crispys"
 
-#command that is used to connect to server and run crista
+# command that is used to connect to server and run crista
 ssh_conect = 'ssh bioseq@powerlogin "module load python/python-anaconda3.6.5 && '
+
+# mafft_path = "/home/josefbrook/miniconda3/envs/crispys/bin/mafft"
+# protdist_path = "/home/josefbrook/miniconda3/envs/crispys/bin/protdist"
+mafft_path = None
+protdist_path = None
--- subgroup_res.py
+++
@@ -1,5 +0,0 @@
-class Subgroup_res:
-	def __init__(self, genes_lst, candidate_lst, name):
-		self.genes_lst = genes_lst
-		self.candidate_lst = candidate_lst
-		self.name = name
