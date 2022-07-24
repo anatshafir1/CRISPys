46f23ff43efc0541d956a3dc6e35cdcb6425a893
JosefBrook <josefbrook@mail.tau.ac.il>
2022-07-24 11:25:22 +0300

    server run arguments simplified

--- server_run.py
+++ server_run.py
@@ -1,4 +1,4 @@
-
+import Stage0
 from Stage0 import *
 import make_tree_display
 import set_cover_greedy
@@ -57,38 +57,6 @@ def parse_arguments(parser_obj: argparse.ArgumentParser):
     :param parser_obj: object for parsing command line strings into python objects
     :return: parsed parameters for the algorithm run
     """
-    parser_obj.add_argument('fasta_file', type=str, metavar='<fasta_file>', help='The output_path to the input fasta '
-                                                                                 'file')
-    parser_obj.add_argument('output_path', type=str, metavar='<output_path>',
-                            help='THe output_path to the directory in which the output files will be written')
-    parser_obj.add_argument('--alg', type=str, default='default', help='Choose "gene_homology" to considering homology')
-    parser_obj.add_argument('--where_in_gene', type=float, default=1,
-                            help='input a number between 0 to 1 in order to ignore targets sites downstream to the '
-                                 'fractional part of the gene')
-    parser_obj.add_argument('--use_thr', '-t', type=int, default=0,
-                            help='0 for using sgRNA to gain maximal gaining score among all of the input genes or 1 for'
-                                 ' the maximal cleavage likelihood only among genes with score higher than the average.'
-                                 ' Default: 0.')
-    parser_obj.add_argument('--omega', '-v', type=float, default=0.43,
-                            help='the value of the threshold. A number between 0 to 1 (included). Default: 0.43')
-    parser_obj.add_argument('--scoring_function', '-s', type=str, default='cfd_funct',
-                            help='the scoring function of the targets. Optional scoring systems are: cfd_funct('
-                                 'default), gold_off, CrisprMIT and CCtop. Additional scoring function may be added '
-                                 'by the user or by request.')
-    parser_obj.add_argument('--min_target_len', '-l', type=int, default=20, help='minimal length of the target site. '
-                                                                                 'Default:20')
-    parser_obj.add_argument('--max_target_len', '-m', type=bool, default=20, help='maximal length of the target site. '
-                                                                                  'Default:20')
-    parser_obj.add_argument('--start_with_g', '-g', type=bool, default=0,
-                            help='1 if the target sites are obligated to start with a G codon or 0 otherwise. '
-                                 'Default: 0.')
-    parser_obj.add_argument('--internal_node_candidates', '-i', type=int, default=10,
-                            help='when choosing the considered homology option, this is the number of sgRNAs designed '
-                                 'for each homology sub-group. Default: 10')
-    parser_obj.add_argument('--max_target_polymorphic_sites', '-ps', type=int, default=12,
-                            help='the maximal number of possible polymorphic sites in a target. Default: 12')
-    parser_obj.add_argument('--pams', type=int, default=0,
-                            help='0 to search NGG pam or 1 to search for NGG and NAG. Default: 0')
     parser_obj.add_argument('--off_targets', type=str, default='Not_selected',
                         help='Name of genome to search for off-target with CRISTA')
     arguments = parser_obj.parse_args()
@@ -97,18 +65,19 @@ def parse_arguments(parser_obj: argparse.ArgumentParser):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    args = parse_arguments(parser)
-    run_server(fasta_file=args.fasta_file,
-               output_path=args.path,
-               alg=args.alg,
-               where_in_gene=args.where_in_gene,
-               use_thr=args.t,
-               Omega=args.v,
-               scoring_function=args.s,
-               min_length=args.l,
-               max_length=args.m,
-               start_with_g=args.g,
-               internal_node_candidates=args.i,
-               max_target_polymorphic_sites=args.ps,
-               pams=args.PAMs,
-               off_targets=args.off_targets)
+    args_main = Stage0.parse_arguments(parser)
+    args_add = parse_arguments(parser)
+    run_server(fasta_file=args_main.fasta_file,
+               output_path=args_main.path,
+               alg=args_main.alg,
+               where_in_gene=args_main.where_in_gene,
+               use_thr=args_main.t,
+               Omega=args_main.v,
+               scoring_function=args_main.s,
+               min_length=args_main.l,
+               max_length=args_main.m,
+               start_with_g=args_main.g,
+               internal_node_candidates=args_main.i,
+               max_target_polymorphic_sites=args_main.ps,
+               pams=args_main.PAMs,
+               off_targets=args_add.off_targets)
