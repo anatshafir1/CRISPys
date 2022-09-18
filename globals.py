651b182d2b4c750ddb864629f45137832d2110a8
udiland <udiland@tauex.tau.ac.il>
2022-09-18 11:11:19 +0300

    add run on cluster

--- globals.py
+++ globals.py
@@ -60,3 +60,29 @@ def set_res_path(path: str):
     """
     global RES_PATH
     RES_PATH = path
+
+
+def createHeaderJob(path, job_name, queue="itaym", ncpu=1, mem=16):
+    """
+    A function to create qsub file with activating crispys conda env
+
+    :param path: path to log files
+    :param job_name: job name
+    :param queue:
+    :param ncpu: cpu number default 1
+    :param mem: memory to use (in gb) default 16
+    :return: a string that can be used to write sh file to run on the cluster (need to add command before running on the cluster)
+    """
+    text = ""
+    text += "#!/bin/bash\n\n"
+    text += "#PBS -S /bin/bash\n"
+    text += "#PBS -r y\n"
+    text += f"#PBS -q {queue}\n"
+    text += "#PBS -N " + job_name + "\n"
+    text += "#PBS -e " + path + "/" + job_name + ".ER" + "\n"
+    text += "#PBS -o " + path + "/" + job_name + ".OU" + "\n"
+    text += "#PBS -l select=ncpus=" + str(ncpu) + ":mem=" + str(mem) + "gb\n"
+    text += "source ~/.bashrc\n"
+    text += "export PATH='$CONDA_PREFIX/bin:$PATH'\n"
+    text += "conda activate /groups/itay_mayrose/udiland/miniconda3/envs/crispys\n"
+    return text
