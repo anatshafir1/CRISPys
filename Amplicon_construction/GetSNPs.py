da1b909ceeee481f4178c16d3fcd7577133b1932
JosefBrook <josefbrook@mail.tau.ac.il>
2024-09-18 13:34:03 +0300

    gap SNPs search fixed 2.0

--- Amplicon_construction/GetSNPs.py
+++ Amplicon_construction/GetSNPs.py
@@ -5,6 +5,19 @@ from collections import Counter
 from SNP_Obj import SNP_Obj
 
 
+def create_idx_nuc_dict(scaffold_to_seq_dict, index, gap_length, distinct_alleles_num):
+
+    idx_nuc_lst = [scaffold_to_seq_dict[allele][index+gap_length] for allele in scaffold_to_seq_dict.keys()]
+    # create "ID" for current position as the dictionary curr_idx_nuc_dict
+    idx_nuc_dict = {"gap_idxs": [i for i in range(distinct_alleles_num) if idx_nuc_lst[i] == "-"],
+                    "nuc_idx": [i for i in range(distinct_alleles_num) if idx_nuc_lst[i] != "-"]}
+    only_nucs_list = [idx_nuc_lst[i] for i in range(distinct_alleles_num) if idx_nuc_lst[i] != "-"]
+    if len(only_nucs_list) > 1:
+        idx_nuc_dict.update({"same_nucs": all(only_nucs_list[0] == nuc for nuc in only_nucs_list)})
+
+    return idx_nuc_dict
+
+
 def create_snps_dict(scaffold_to_seq_dict: Dict[str, str], distinct_alleles_num: int, primer_length: int) -> List[SNP_Obj]:
     """
 
@@ -19,12 +32,12 @@ def create_snps_dict(scaffold_to_seq_dict: Dict[str, str], distinct_alleles_num:
     seq_len = len(list(scaffold_to_seq_dict.values())[0])
     index = 0
     while index < seq_len:
-        curr_index_nuc_list = [scaffold_to_seq_dict[allele][index] for allele in scaffold_to_seq_dict.keys()]
-        counts = Counter(curr_index_nuc_list)
-        if all(curr_index_nuc_list[0] == nuc for nuc in curr_index_nuc_list):  # no SNP in current index
+        curr_idx_nuc_lst = [scaffold_to_seq_dict[allele][index] for allele in scaffold_to_seq_dict.keys()]
+        counts = Counter(curr_idx_nuc_lst)
+        if all(curr_idx_nuc_lst[0] == nuc for nuc in curr_idx_nuc_lst):  # no SNP in current index
             index += 1
-        elif all(curr_index_nuc_list[i] != "-" for i in range(distinct_alleles_num)):  # no indel in current index
-            unique_alleles = {all_alleles_ids_list[i] for i, val in enumerate(curr_index_nuc_list) if counts[val] == 1}
+        elif all(curr_idx_nuc_lst[i] != "-" for i in range(distinct_alleles_num)):  # no indel in current index
+            unique_alleles = {all_alleles_ids_list[i] for i, val in enumerate(curr_idx_nuc_lst) if counts[val] == 1}
             snp = SNP_Obj(index, unique_alleles)
             if snp.position > primer_length:
                 result_list.append(snp)
@@ -34,18 +47,25 @@ def create_snps_dict(scaffold_to_seq_dict: Dict[str, str], distinct_alleles_num:
             if snp.position > primer_length:
                 result_list.append(snp)
             index += 1
-        else:  # indel/s in current position with same nucleotide/s in other alleles
+        else:  # indel/s in current position with same nucleotide/s in other alleles. ex. ('A', '-', 'A'), ('G', '-', '-')
             gap_length = 1
             if distinct_alleles_num == 2:
                 curr_distinct_allele = all_alleles_ids_set
             else:
-                curr_distinct_allele = {all_alleles_ids_list[i] for i, val in enumerate(curr_index_nuc_list) if counts[val] == 1}
+                curr_distinct_allele = {all_alleles_ids_list[i] for i, val in enumerate(curr_idx_nuc_lst) if counts[val] == 1}
             if index+gap_length < seq_len:
-                next_index_nuc_list = [scaffold_to_seq_dict[allele][index+gap_length] for allele in scaffold_to_seq_dict.keys()]
-                while curr_index_nuc_list == next_index_nuc_list and index+gap_length < seq_len:
+                # create "ID" for current position as the dictionary curr_idx_nuc_dict
+                curr_idx_nuc_dict = create_idx_nuc_dict(scaffold_to_seq_dict, index, 0, distinct_alleles_num)
+                # create "ID" for next position as the dictionary next_index_nuc_dict
+                next_index_nuc_dict = create_idx_nuc_dict(scaffold_to_seq_dict, index, gap_length, distinct_alleles_num)
+                # find gap length by comparing the "ID" dictionaries between positions
+                while curr_idx_nuc_dict == next_index_nuc_dict and index+gap_length < seq_len:
                     gap_length += 1
-                    next_index_nuc_list = [scaffold_to_seq_dict[allele][index + gap_length] for allele in
-                                           scaffold_to_seq_dict.keys()]
+                    if index+gap_length < seq_len:
+                        # create "ID" for next position as the dictionary next_index_nuc_dict
+                        next_index_nuc_dict = create_idx_nuc_dict(scaffold_to_seq_dict, index, gap_length, distinct_alleles_num)
+                    else:
+                        break
                 snp = SNP_Obj(index, curr_distinct_allele, gap_length)
                 if snp.position > primer_length:
                     result_list.append(snp)
